fa5329f7adc7937350145497a757068e0f76b23d
harshs038 <harsh.s.tech@gmail.com>
2025-04-10 18:37:15 +0530

    add greet

--- sqrt.c
+++ sqrt.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 int main(){
    
-    printf("root of the number is : %d", rut());
+    printf("root of the number is : %d\n", rut());
+    greet();
     return 0;
 }
  
@@ -11,4 +12,8 @@ int rut(){
   scanf("%d", &a);
     int root = sqrt(a);  
     return root ;
+}
+
+void greet(){
+    printf("Thanks , good bye.");
 }
