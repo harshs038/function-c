68f81117263a528144b8bb2a9d4ef6e146adc3b9
harshs038 <harsh.s.tech@gmail.com>
2025-04-10 18:09:56 +0530

    sum of two

---
+++ sumof2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+  int main(){
+    int a , b ;
+    printf("Enter A :");
+    scanf("%d", &a);
+    printf("Enter B :");
+    scanf("%d", &b);
+    int sum= add(a,b);
+    printf("%d", sum);
+    return 0;
+  }
+  int add(int a , int b ){
+    return a+b;
+  }
