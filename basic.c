80f19e15a3fa5efe84e401a99b6436ebb7dafbb8
harshs038 <harsh.s.tech@gmail.com>
2025-04-10 18:04:55 +0530

    function

---
+++ basic.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+  
+void Name(){
+    printf("My name is harsh\n");
+    Greet();
+    return ; 
+}
+
+void Greet(){
+    printf("Thank you");
+    return ; 
+}
+
+int main(){
+
+   Name();
+    return 0 ;
+}
